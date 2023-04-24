cd6876d2ab4bdcb141f171c258aa699f8310652f
george <gelkik@gmail.com>
2023-04-24 14:05:10 -0400

    Changed icons for languages

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,12 +1,12 @@
 import React from "react";
 import DevIcon from "devicon-react-svg";
-// import { Box } from '@mui/material'
+import { Box } from '@mui/material'
 // import * as icon from 'devicon/icons'
 
 // Local Data
 // import data from "../../data/portfolio.json";
 
-const AboutMe = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
+const AboutMe = ({ }) => {
   // const router = useRouter();
   // const { theme, setTheme } = useTheme();
   // const [mounted, setMounted] = useState(false);
@@ -43,29 +43,41 @@ const AboutMe = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
           Architect as well as Cloud Practitioner. Along with my technical coding skills, I am a graduate of Electrical Engineering,
           completing my Masters Degree in May of 2021, focusing on telecommunications, computer networking and security,
           and power systems.
-          <h4>
+          <br />
+          <br />
+          <div>
             I am passionate to use my knowledge and knack for problem-solving in Full Stack Development in the 
             tech industry, and am eager to continuously learn new things!
-          </h4>
+          </div>
         </div>
         <div className="about-me-skills">
           <h2>
             Coding Languages and Frameworks
           </h2>
           <div>
-            <DevIcon icon="react" style={devIconStyle} viewBox="0 0 32 32"/>
+            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg'/>
+            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg'/>
+            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-plain-wordmark.svg'/>
+            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg'/>
+            {/* <DevIcon icon="react" style={devIconStyle} viewBox="0 0 32 32"/>
             <DevIcon icon="python" style={devIconStyle} viewBox="0 0 32 32"/>
             <DevIcon icon="javascript" style={devIconStyle} viewBox="0 0 32 32"/>
-            <DevIcon icon="html5" style={devIconStyle} viewBox="0 0 32 32"/>
+            <DevIcon icon="html5" style={devIconStyle} viewBox="0 0 32 32"/> */}
             <div>
-            <DevIcon icon="css3" style={devIconStyle} viewBox="0 0 32 32"/>
+            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg'/>
+            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg'/>
+            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg'/>
+            {/* <DevIcon icon="css3" style={devIconStyle} viewBox="0 0 32 32"/>
             <DevIcon icon="nodejs" style={devIconStyle} viewBox="0 0 32 32"/>
-            <DevIcon icon="github" style={devIconStyle} viewBox="0 0 32 32"/>
+            <DevIcon icon="github" style={devIconStyle} viewBox="0 0 32 32"/> */}
             </div>
-            <div>
-            <DevIcon icon="django" style={devIconStyle} viewBox="0 0 32 32"/>
+            <div className="about-me-row3">
+            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'/>
+            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg'/>
+            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-plain-wordmark.svg'/>
+            {/* <DevIcon icon="django" style={devIconStyle} viewBox="0 0 32 32"/>
             <DevIcon icon="sqllite" style={devIconStyle} viewBox="0 0 32 32"/>
-            <DevIcon icon="aws" style={devIconStyle} viewBox="0 0 32 32"/>
+            <DevIcon icon="aws" style={devIconStyle} viewBox="0 0 32 32"/> */}
             </div>
           </div>
         </div>
