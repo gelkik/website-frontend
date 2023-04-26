595e01e48de807b0d8c1029a0487b632db0b3aed
george <gelkik@gmail.com>
2023-04-26 11:43:33 -0400

    Added projects

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,6 @@
 import React from "react";
-import DevIcon from "devicon-react-svg";
-import { Box } from '@mui/material'
+// import DevIcon from "devicon-react-svg";
+// import { Box } from '@mui/material'
 // import * as icon from 'devicon/icons'
 
 // Local Data
@@ -54,27 +54,27 @@ const AboutMe = ({ }) => {
           <h2>
             Coding Languages and Frameworks
           </h2>
-          <div>
-            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg'/>
-            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg'/>
-            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-plain-wordmark.svg'/>
-            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg'/>
+          <div className="about-me-logos">
+            <img style = {{width: '100px'}} alt='AWS' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg'/>
+            <img style = {{width: '100px'}}alt='React'src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg'/>
+            <img style = {{width: '100px'}}alt='Python'src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-plain-wordmark.svg'/>
+            <img style = {{width: '100px'}}alt='JavaScript'src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg'/>
             {/* <DevIcon icon="react" style={devIconStyle} viewBox="0 0 32 32"/>
             <DevIcon icon="python" style={devIconStyle} viewBox="0 0 32 32"/>
             <DevIcon icon="javascript" style={devIconStyle} viewBox="0 0 32 32"/>
             <DevIcon icon="html5" style={devIconStyle} viewBox="0 0 32 32"/> */}
             <div>
-            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg'/>
-            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg'/>
-            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg'/>
+            <img style = {{width: '100px'}}alt='Node.JS'src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg'/>
+            <img style = {{width: '100px'}}alt='HTML'src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg'/>
+            <img style = {{width: '100px'}}alt='CSS'src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg'/>
             {/* <DevIcon icon="css3" style={devIconStyle} viewBox="0 0 32 32"/>
             <DevIcon icon="nodejs" style={devIconStyle} viewBox="0 0 32 32"/>
             <DevIcon icon="github" style={devIconStyle} viewBox="0 0 32 32"/> */}
             </div>
             <div className="about-me-row3">
-            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'/>
-            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg'/>
-            <img style = {{width: '150px'}}src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-plain-wordmark.svg'/>
+            <img style = {{width: '100px'}}alt='Github'src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'/>
+            <img style = {{width: '100px'}}alt='Django'src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg'/>
+            <img style = {{width: '100px'}}alt='SQLite'src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-plain-wordmark.svg'/>
             {/* <DevIcon icon="django" style={devIconStyle} viewBox="0 0 32 32"/>
             <DevIcon icon="sqllite" style={devIconStyle} viewBox="0 0 32 32"/>
             <DevIcon icon="aws" style={devIconStyle} viewBox="0 0 32 32"/> */}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,96 @@
-import React from "react";
+import React, { useRef,useState } from "react";
 // import Header from './Header'
 // import EmailForm from './EmailForm'
 // import AboutMe from './AboutMe'
+import myvideo from '../video/DataDunk Demo.mp4'
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Fade from '@mui/material/Fade';
+import Typography from '@mui/material/Typography';
 
 function Projects() {
+    const playerRef = useRef(null);
+    const [showVid,setShowVid] = useState(false)
+
+    function handleClose(){
+        setShowVid(()=>!showVid)
+    }
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '85vw',
+        bgcolor: 'black',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
     return (
         <div className= "projects">
-            <h1>
-                Projects
-            </h1> 
+            Projects
+            <br />
+            <br />
+                {/* <ReactPlayer ref={playerRef} url={'C:\Users\gelki\Videos\Captures/DataDunk Demo'} controls={true} /> */}
+                
+                {showVid ? 
+                <Modal
+                    open={showVid}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        overflow:'scroll'
+                        }}
+                >
+                <Fade in={showVid}>
+                    <Box sx={style}> 
+                    <h2 style={{color:'white',textAlign:'right'}}onClick={handleClose}>X</h2>
+                    <Typography id="transition-modal-title" variant="h4" component="h2" style={{color: 'orange',fontFamily:'monospace'}}>
+                        DataDunk
+                        <Typography id="transition-modal-title" variant="h6" component="h2" style={{color: 'white',fontFamily:'monospace'}}>
+                    <a style={{color: 'white',textDecoration:'none'}} href='https://github.com/gelkik/nba-stats'>Github</a>
+                    </Typography>
+                    </Typography>
+                    <video style={{
+                        display: "flex",
+                        alignItems: "center",
+                        height: "75%",
+                        width:'75%',
+                        }} 
+                        controls preload="true">
+                        {/* <button onClick={handleClose}>Close</button> */}
+                        <source src={myvideo} type='video/mp4'/>
+                    </video>
+                    <Typography id="transition-modal-title" variant="h6" component="h2" style={{color: 'white',fontFamily:'monospace'}}>
+                    Created a real-time NBA analysis application that gives insights into NBA teams’ performances in order to give
+                    users the opportunity to make data-driven decisions when betting. Shows all upcoming bets as well as their odds.
+                    </Typography>
+                    <Typography id="transition-modal-title" variant="h6" component="h2" style={{color: 'white',fontFamily:'monospace'}}>
+                    - Frontend: React, Node.JS
+                    </Typography>
+                    <Typography id="transition-modal-title" variant="h6" component="h2" style={{color: 'white',fontFamily:'monospace'}}>
+                    - Backend: Python, Flask, REST, SQLAlchemy
+                    </Typography>
+                    </Box>
+                </Fade>
+                
+                </Modal>
+                :
+                <div className ='projects-1' style={{cursor: 'pointer'}} onClick={()=>setShowVid(true)}>
+                    
+                    <img width='100px' alt="DataDunk" src='https://cdn1.iconfinder.com/data/icons/education-176/32/sport_basketball_ball_game_play-512.png'/>
+                    <br />
+                    <div style={{color:'orange'}}>DataDunk</div>
+                </div>
+                }
+                <br />
+            <div className ='projects-2'>
+                <a style={{color: 'red',textDecoration:'none'}}href="https://github.com/gelkik/Dog-Shelter-Web">Roger's Dog Rescue</a>
+            </div>
         </div>
     )
 }
